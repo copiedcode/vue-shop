1cb35a855ecdc54be2555b355747a5de5f6777a6
copiedcode <raphael.webdesign@outlook.com>
2020-05-12 21:05:57 +0200

    fixing cart

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,10 @@
     :cart="cart"
     :cartQty="cartQty"
     :cartTotal="cartTotal"
-    @toggle="toggleSliderStatus"></navbar>
+    @toggle="toggleSliderStatus"
+    @delete="deleteItem">
+
+    </navbar>
     <price-slider :slider-status="sliderStatus" :maximum.sync="maximum"></price-slider>
     <product-list :maximum="maximum" :products="products" @add="addItem"></product-list>
   </div>
@@ -33,6 +36,22 @@ export default {
       cart: []
     };
   },
+  computed: {
+    cartTotal: function() {
+      let sum = 0;
+      for (let key in this.cart){
+        sum = sum+(this.cart[key].product.price * this.cart[key].qty);
+      }
+      return sum;
+    },
+    cartQty: function() {
+      let qty = 0;
+      for (let key in this.cart){
+        qty = qty+this.cart[key].qty;
+      }
+      return qty;
+    }
+  },
   methods: {
     toggleSliderStatus(){
       this.sliderStatus = !this.sliderStatus;
@@ -52,6 +71,13 @@ export default {
         this.cart[whichProduct].qty++;
       } else {
         this.cart.push({product: product, qty: 1})
+      }
+    },
+    deleteItem: function(id) {
+      if(this.cart[id].qty > 1) {
+        this.cart[id].qty--;
+      } else {
+        this.cart.splice(id,1);
       }
     }
   },
