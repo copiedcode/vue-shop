6dde41816f1c7458187d323f9acd4f4a2d4e7ef0
copiedcode <raphael.webdesign@outlook.com>
2020-05-17 22:07:21 +0200

    add routing links

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -2,11 +2,13 @@
     <div>
         <h1>Checkout</h1>
         <table class="table table-hover" v-if="cart.length">
-            <caption class="text-right h3">
+            <caption class="text-right h4">
                 <b>Total:</b>
-                <price class="d-block text-success font-weight-light"
+                <price class="d-block text-success font-weight-bold"
                        :value="Number(cartTotal)"></price>
+                <p style="font-size: 0.5em" class="text-right">incl. MwSt. <br> shipping excl. </p>
             </caption>
+
             <thead>
             <tr>
                 <th scope="col">Item</th>
@@ -25,6 +27,10 @@
             </tr>
             </tbody>
         </table>
+
+        <router-link class="btn btn-outline-warning text-dark font-weight-bold" to="/cart">Back to the Cart</router-link>
+        <router-link class="btn btn-outline-success text-dark font-weight-bold float-right" to="/paying">Proceed</router-link>
+
     </div>
 </template>
 
